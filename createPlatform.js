f9bf7cefaefb3aa7e973b6f9e1455fd95dd8d102
Noel Carino <leononirac1992@gmail.com>
2020-01-19 17:57:10 -0800

    1/19 | Started working on button to load elements in body on click

--- createPlatform.js
+++ createPlatform.js
@@ -27,26 +27,81 @@ class LeetCode{
 
     }
     loadBody(){
-        var buttonClass = "btn btn-danger px-3";
+        var buttonClass = "btn btn-danger px-3 mx-3";
         var button = $("<button>")
                         .attr('id','buttonTest')
                         .addClass(buttonClass)
-                        .text("Button Test");
+                        .text("Load Easy Problems");
+        var button2 = $("<button>")
+                        .attr('id','buttonTest2')
+                        .addClass(buttonClass)
+                        .text("Load Medium Problems");
+        var button3 = $("<button>")
+                        .attr('id','buttonTest3')
+                        .addClass(buttonClass)
+                        .text("Load Hard Problems");
+
         var bodyClass = "container-fluid bg-light mx-auto py-5";
         var bodyElement = $("<div>")
                             .attr('id','bodyTest')
                             .addClass(bodyClass)
-                            .text("hello")
-                            .append(button);
+                            .append(button)
+                            .append(button2)
+                            .append(button3);
 
         $(this.dom.container).append(bodyElement);
     }
     addYellowBlock(){
-        var yellowBlock = $("<div>").addClass("container-fluid p-3").text("yellow block");
-
+        var yellowButton = $("<button>")
+                                .text("yellow button")
+                                .addClass("btn btn-warning mb-3")
+                                .on("click", function(){
+                                    console.log("removed");
+                                })
+                                
+        var yellowBlock = $("<div>")
+                                .addClass("container-fluid p-3")
+                                .append(yellowButton)
+                                .on("click", function(){
+                                    $(this).remove();
+                                });
+        $("#bodyTest").append(yellowBlock);
+    }
+    addBlueBLock(){
+        var yellowButton = $("<button>")
+                                .text("blue button")
+                                .addClass("btn btn-primary mb-3")
+                                .on("click", function(){
+                                    console.log("removed");
+                                })
+                                
+        var yellowBlock = $("<div>")
+                                .addClass("container-fluid p-3")
+                                .append(yellowButton)
+                                .on("click", function(){
+                                    $(this).remove();
+                                });
+        $("#bodyTest").append(yellowBlock);
+    }
+    addGreenBlock(){
+        var yellowButton = $("<button>")
+                                .text("Green button")
+                                .addClass("btn btn-success mb-3")
+                                .on("click", function(){
+                                    console.log("removed");
+                                })
+                                
+        var yellowBlock = $("<div>")
+                                .addClass("container-fluid p-3")
+                                .append(yellowButton)
+                                .on("click", function(){
+                                    $(this).remove();
+                                });
         $("#bodyTest").append(yellowBlock);
     }
     addEventListener(){
         $("#buttonTest").on('click', this.addYellowBlock);
+        $("#buttonTest2").on('click', this.addBlueBLock);
+        $("#buttonTest3").on('click', this.addGreenBlock);
     }
 }
