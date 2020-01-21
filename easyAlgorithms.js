e52cb16f6990d55da98490d5ae3a8e95fff10c9d
Noel Carino <leononirac1992@gmail.com>
2020-01-20 21:06:14 -0800

    1/20 | When I click an easy problem button, I can render images

--- easyAlgorithms.js
+++ easyAlgorithms.js
@@ -1,14 +1,13 @@
 class EasyAlgorithms{
     loadEasyAlgorithms(array){
-        console.log(array);
         var algorithmButtonClass = "btn btn-primary btn-block text-light  mb-5 p-2 shadow";
-        var algorithmButtons,buttonContainer;
-        var algorithmButtonContainerClass = "container-fluid row p-3 mx-auto justify-content-around"
+        var algorithmButtons, buttonContainer;
+        var algorithmButtonContainerClass = "container-fluid row px-1 py-3 mx-auto justify-content-around"
         var algorithmButtonContainer = $("<div>").addClass(algorithmButtonContainerClass);
 
         for (var i = 0; i < array[0].length; i++){
             buttonContainer = $("<div>")
-                                    .addClass("container-fluid col-8 px-3")
+                                    .addClass("container-fluid col-12 px-0")
                                     .appendTo(algorithmButtonContainer);
             algorithmButtons = $("<button>")
                                     .addClass(algorithmButtonClass)
