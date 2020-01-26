991435bb0865a0b64c739fbcc6614b370f8bddaf
Noel Carino <leononirac1992@gmail.com>
2020-01-25 16:09:30 -0800

    1/25 | Submitting '#771 - Jewels and Stones' to Solutions Library

--- AcceptedLCSolutions/easy.js
+++ AcceptedLCSolutions/easy.js
@@ -273,3 +273,46 @@ function fizzBuzz(n){
 function toLowerCase(str){
     return str.toLowerCase();
 }
+
+/****************************************************************
+    Date: 1/25/2020
+    LC: #771 - Easy - Jewels and Stones
+    Description:
+        You're given strings 'J' representing the types of stones that are
+        jewels, and 'S' representing the stones you have. Each character in
+        'S' is a type of stone you have. You want to now how many of the stones you have
+        are also jewels.
+
+        The letters in 'J' are guranteed distinct, and all characters in 'J' and 'S' are
+        letters. Letters are case sensitive, so 'a' is considered a different type of stone
+        from 'A'
+
+    Example 1:
+        Input: J = 'aA', S = 'aAAbbbb'
+        Output: 3
+
+    Example 2:
+        Input: J = 'z', S = 'ZZ'
+        Output: 0
+
+    Note:
+        - 'S' and 'J' will consist of letters and have length at most 50
+        - The characters in 'J' are distinct
+****************************************************************/
+
+function jewelsAndStones(jewels, stones){
+    var jewelCount = 0;
+    for (var i = 0; i < jewels.length;i++) {
+      for (var j = 0; j < stones.length; j++){
+        if (jewels[i] === stones[j]) jewelCount++;
+      }
+    }
+  return jewelCount;
+}
+
+
+
+
+/****************************************************************
+
+****************************************************************/
