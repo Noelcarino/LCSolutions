a20b04dc1260f0403c9efe7b31362688d1d354d8
Noel Carino <leononirac1992@gmail.com>
2020-01-27 04:01:13 -0800

    1/27 | Submitting '#66 - Plus One' to solutions library

--- AcceptedLCSolutions/easy.js
+++ AcceptedLCSolutions/easy.js
@@ -83,6 +83,28 @@ function palindromeNumber(integer){
 }
 
 /****************************************************************
+    Date: 1/27/2020
+    LC: #0066 - Easy - Plus One
+    Description:
+        Given a non-empty array of digits representing a non-negative integer, 
+        plus one to the integer.
+
+    The digits are stored such that the most significant digit is at the head of the list, 
+    and each element in the array contain a single digit.
+
+    You may assume the integer does not contain any leading zero, except the number 0 itself.
+****************************************************************/
+
+function plusOne(digits){
+    for (var i = digits.length - 1; i >=0; i--){
+        if(++digits[i] > 9) digits[i] = 0;
+        else return digits;
+    }
+    digits.unshift(1);
+    return digits;
+}
+
+/****************************************************************
     Date: 1/26/2020
     LC: #0083 - Easy - Remove Duplicates from Sorted List
     Description:
