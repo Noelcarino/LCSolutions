bbbba9b5dce1963596e87dd6eeb711b258bda3ff
Noel Carino <leononirac1992@gmail.com>
2020-02-07 15:49:30 -0800

    2/7 | Setting up LCVersion 3, presenting solutions in react version

---
+++ lcsolutions_version3/src/components/lcsolutions.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+export default class LCSolutions extends React.Component {
+    render(){
+        return (
+            <div>
+                THESE ARE MY SOLUTIONS :D
+            </div>
+        )
+    }
+}
