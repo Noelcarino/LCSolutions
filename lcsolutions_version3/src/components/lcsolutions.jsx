4984bb16e51ec578843196883ad851f130a99c64
Noel Carino <leononirac1992@gmail.com>
2020-02-07 16:05:33 -0800

    2/7 | Figured a way to present solutions to DOM

--- lcsolutions_version3/src/components/lcsolutions.jsx
+++ lcsolutions_version3/src/components/lcsolutions.jsx
@@ -3,11 +3,49 @@ import React from 'react';
 
 
 export default class LCSolutions extends React.Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            easySolutions: {
+                problems1: {
+                    id: 1,
+                    arrayFormat: [
+                        'LC: #001 - Easy - Two Sum',
+                        ' ',
+                        'function twoSum(nums,target){',
+                        '   var twoElements = [];',
+                        '   for (var i = 0; i < nums.length; i++{',
+                        '       for (var j = 0; j < nums.length; j++){',
+                        '           if (i !== j) {',
+                        '               if(nums[i] + nums[j] == target) {',
+                        "                   twoElements.push(i);",
+                        '                   twoElements.push(j);',
+                        '                   return twoElements;',
+                        '               }',
+                        '           }',
+                        '       }',
+                        '   }',
+                        '   return false;',
+                        '}'
+                    ]
+                }
+            }
+        }
+    }
     render(){
+        console.log(this.state.easySolutions.problems1.arrayFormat);
         return (
-            <div>
+            <div className="container-fluid bg-dark text-light">
                 THESE ARE MY SOLUTIONS :D
+                <ul className="list-group bg-dark">
+                    {this.state.easySolutions.problems1.arrayFormat.map((line, index) => {
+                        return (
+                            <li key={index} className="bg-dark list-group-item border-0 py-0">{line.replace(/ /g, "\u00a0")}</li>
+                        )
+                    })}
+                </ul>
             </div>
         )
     }
 }
+
