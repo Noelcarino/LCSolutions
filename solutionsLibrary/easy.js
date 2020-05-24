a23cfd1f2d64148208e071153c8d2a0c72a34b1e
Noel Carino <leononirac1992@gmail.com>
2020-05-23 21:33:37 -0700

    05/23 | completed #0121 - Best Time to Buy and Sell Stock

--- solutionsLibrary/easy.js
+++ solutionsLibrary/easy.js
@@ -230,6 +230,42 @@ function pascalsTriangle(numRows){
     }
     return array2;
 };
+
+/****************************************************************
+    Date: 05/23/2020
+    LC: #0121 - Easy - Best Time to Buy and Sell Stock
+    Description:
+        Say you have an array for which the ith element is the price of a given stock on day i.
+
+        If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+        Note that you cannot sell a stock before you buy one.
+    
+    Example 1:
+        Input: [7,1,5,3,6,4]
+        Output: 5
+        Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+                    Not 7-1 = 6, as selling price needs to be larger than buying price.
+
+****************************************************************/
+
+// Attempt 1 - Faster than 5.07% of JavaScript online Submissions
+var maxProfit = function(prices) {
+    var profit = 0;
+    for (var i = 0; i < prices.length; i++){
+        for (var j = 0; j < prices.length; j++){
+            if (j > i){
+                if (prices[i] < prices[j]){
+                    if (prices[j] - prices[i] > profit){
+                        profit = prices[j] - prices[i];
+                    }
+                }
+            }
+        }
+    }
+    return profit;
+};
+
 /****************************************************************
     Date: 1/20/2020
     LC: #125 - Easy - Valid Palindrome
