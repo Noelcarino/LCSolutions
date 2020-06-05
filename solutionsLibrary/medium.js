b0f0cd0934cea30101ae3511d9f078da63cef73a
Noel Carino <leononirac1992@gmail.com>
2020-06-04 22:00:44 -0700

    6/04 | completed #0056 - Merge Intervals

--- solutionsLibrary/medium.js
+++ solutionsLibrary/medium.js
@@ -94,6 +94,31 @@ var searchRange = function(nums, target) {
 };
 
 /****************************************************************
+    Date: 06/04/2020
+    LC: #0056 - Medium - Merge Intervals
+    Description:
+        Given a collection of intervals, merge all overlapping intervals.
+
+    Example:
+        Input: [[1,3],[2,6],[8,10],[15,18]]
+        Output: [[1,6],[8,10],[15,18]]
+        Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+
+****************************************************************/
+
+var merge = function(intervals) {
+    intervals.sort(function(a,b){ return a[0] - b[0]})
+    for (var i = 0; i < intervals.length - 1; i++){
+        if (intervals[i][1] > intervals[i+1][0] || intervals[i][1] === intervals[i+1][0]){
+            if (intervals[i][1] < intervals[i+1][1]) intervals[i][1] = intervals[i+1][1];
+            intervals.splice(i+1,1);
+            --i;
+        }
+    }
+    return intervals;
+};
+
+/****************************************************************
     Date: 06/03/2020
     LC: #0073 - Medium - Set Matrix Zeroes
     Description:
